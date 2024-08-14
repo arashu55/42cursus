73e8188e6e0aa9a81c9c91304be9f554c07f1146
Arai Shu <ashu@c1r6s7.42tokyo.jp>
2024-08-14 20:17:45 +0900

    テスト用メイン関数をテストフォルダーに分割し鬱フォルダーに分割し移動しました

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -6,54 +6,11 @@
 /*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 00:22:09 by ashu              #+#    #+#             */
-/*   Updated: 2024/08/09 22:18:17 by ashu             ###   ########.fr       */
+/*   Updated: 2024/08/14 20:16:01 by ashu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- int ft_isalpha(int c)
- {
-    return (ft_isupper (c)|| ft_islower(c));
- }
-
-#include <stdio.h>
-#include <ctype.h>
-int main()
+int	ft_isalpha(int c)
 {
-   printf("%d\n", ft_isalpha(-10));
-   printf("%d\n", ft_isalpha(-1));
-   printf("%d\n", ft_isalpha(0));
-   printf("%d\n", ft_isalpha(' '));
-   printf("%d\n", ft_isalpha('@'));
-   printf("%d\n", ft_isalpha('A'));
-   printf("%d\n", ft_isalpha('M'));
-   printf("%d\n", ft_isalpha('Z'));
-   printf("%d\n", ft_isalpha('['));
-   printf("%d\n", ft_isalpha('`'));
-   printf("%d\n", ft_isalpha('a'));
-   printf("%d\n", ft_isalpha('m'));
-   printf("%d\n", ft_isalpha('z'));
-   printf("%d\n", ft_isalpha('{'));
-   printf("%d\n", ft_isalpha('}'));
-   printf("%d\n", ft_isalpha(127));
-   printf("%d\n", ft_isalpha(128));
-   printf("%d\n", ft_isalpha(150));
-
-   printf("%d\n", isalpha(-10));
-   printf("%d\n", isalpha(-1));
-   printf("%d\n", isalpha(0));
-   printf("%d\n", isalpha(' '));
-   printf("%d\n", isalpha('@'));
-   printf("%d\n", isalpha('A'));
-   printf("%d\n", isalpha('M'));
-   printf("%d\n", isalpha('Z'));
-   printf("%d\n", isalpha('['));
-   printf("%d\n", isalpha('`'));
-   printf("%d\n", isalpha('a'));
-   printf("%d\n", isalpha('m'));
-   printf("%d\n", isalpha('z'));
-   printf("%d\n", isalpha('{'));
-   printf("%d\n", isalpha('}'));
-   printf("%d\n", isalpha(127));
-   printf("%d\n", isalpha(128));
-   printf("%d\n", isalpha(150));
+   return (ft_isupper (c)|| ft_islower(c));
 }
---
+++ libft/test/test_isalpha.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_isalpha.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/14 19:57:29 by ashu              #+#    #+#             */
+/*   Updated: 2024/08/14 19:57:49 by ashu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <ctype.h>
+int main()
+{
+   printf("%d\n", ft_isalpha(-10));
+   printf("%d\n", ft_isalpha(-1));
+   printf("%d\n", ft_isalpha(0));
+   printf("%d\n", ft_isalpha(' '));
+   printf("%d\n", ft_isalpha('@'));
+   printf("%d\n", ft_isalpha('A'));
+   printf("%d\n", ft_isalpha('M'));
+   printf("%d\n", ft_isalpha('Z'));
+   printf("%d\n", ft_isalpha('['));
+   printf("%d\n", ft_isalpha('`'));
+   printf("%d\n", ft_isalpha('a'));
+   printf("%d\n", ft_isalpha('m'));
+   printf("%d\n", ft_isalpha('z'));
+   printf("%d\n", ft_isalpha('{'));
+   printf("%d\n", ft_isalpha('}'));
+   printf("%d\n", ft_isalpha(127));
+   printf("%d\n", ft_isalpha(128));
+   printf("%d\n", ft_isalpha(150));
+
+   printf("%d\n", isalpha(-10));
+   printf("%d\n", isalpha(-1));
+   printf("%d\n", isalpha(0));
+   printf("%d\n", isalpha(' '));
+   printf("%d\n", isalpha('@'));
+   printf("%d\n", isalpha('A'));
+   printf("%d\n", isalpha('M'));
+   printf("%d\n", isalpha('Z'));
+   printf("%d\n", isalpha('['));
+   printf("%d\n", isalpha('`'));
+   printf("%d\n", isalpha('a'));
+   printf("%d\n", isalpha('m'));
+   printf("%d\n", isalpha('z'));
+   printf("%d\n", isalpha('{'));
+   printf("%d\n", isalpha('}'));
+   printf("%d\n", isalpha(127));
+   printf("%d\n", isalpha(128));
+   printf("%d\n", isalpha(150));
+}
