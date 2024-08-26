3cd788f7a5412e6304b7bed2dedb392206301bdf
Arai Shu <ashu@c3r5s7.42tokyo.jp>
2024-08-26 19:25:12 +0900

    ヘッダーファイルにプロトタイプ宣言を追加　

--- libft/libft.h
+++ libft/libft.h
@@ -6,11 +6,16 @@
 /*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 02:51:41 by ashu              #+#    #+#             */
-/*   Updated: 2024/08/15 02:55:43 by ashu             ###   ########.fr       */
+/*   Updated: 2024/08/26 19:24:22 by ashu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 #define LIBFT_H
+
+int ft_isupper(int c);
+int ft_islower(int c);
+int	ft_isalpha(int c);
+int ft_isdigit(int c);
 
 #endif
