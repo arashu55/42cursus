17ba3bc4b3f7b5c6e941523454b23611e9eeb68e
Arai Shu <ashu@c3r5s7.42tokyo.jp>
2024-08-26 19:31:50 +0900

    isdigitのmain関数をtest_digit.cに移動　

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 16:05:55 by ashu              #+#    #+#             */
-/*   Updated: 2024/06/06 16:36:23 by ashu             ###   ########.fr       */
+/*   Updated: 2024/08/26 19:31:17 by ashu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,4 @@ int ft_isdigit(int c)
         return(1);
     }
     return(0);
-}
-
-#include <ctype.h>
-#include <stdio.h>
-
-int main()
-{
-    int i;
-    i = 0;
-    while (i != 127)
-    {
-        printf("original %d is %d\n", i, ft_isdigit(i));
-        printf("my %d is %d\n" , i, isdigit(i));
-        i++;
-    }
-    printf("%d\n", ft_isdigit('0'));
-    printf("%d\n", ft_isdigit('a'));
-    printf("%d\n", isdigit('0'));
-    printf("%d\n", isdigit('a'));
 }
