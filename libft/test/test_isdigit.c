319c8efbed2f0d3b6b3c1c78ab9d5dfd761bfe4b
Arai Shu <ashu@c3r2s2.42tokyo.jp>
2024-08-27 19:20:28 +0900

    よくわからんことがおきた

---
+++ libft/test/test_isdigit.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_isdigit.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 19:26:44 by ashu              #+#    #+#             */
+/*   Updated: 2024/08/27 19:19:10 by ashu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    int i;
+    i = 0;
+    while (i != 127)
+    {
+        printf("original %d is %d\n", i, ft_isdigit(i));
+        printf("my %d is %d\n" , i, isdigit(i));
+        i++;
+    }
+    printf("%d\n", ft_isdigit('0'));
+    printf("%d\n", ft_isdigit('a'));
+    printf("%d\n", isdigit('0'));
+    printf("%d\n", isdigit('a'));
+}
