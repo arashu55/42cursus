f5c04ca714983e60c3aba4bfe326726ff4e24881
Arai Shu <ashu@c3r2s2.42tokyo.jp>
2024-08-27 19:15:32 +0900

    関数にmain関数があったところを削除

--- libft/ft_islower.c
+++ libft/ft_islower.c
@@ -6,7 +6,7 @@
 /*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 09:35:37 by ashu              #+#    #+#             */
-/*   Updated: 2024/08/27 18:39:04 by ashu             ###   ########.fr       */
+/*   Updated: 2024/08/27 19:14:37 by ashu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,4 @@ int ft_islower(int c)
         return(1);
     }
     return(0);
-}
-
-#include <stdio.h>
-#include <ctype.h>
-
-int main()
-{
-    printf("%d\n", ft_islower('a'));
-    printf("%d\n", ft_islower('A'));
-    printf("%d\n", islower('a'));
-    printf("%d\n", islower('A'));
 }
--- libft/ft_isupper.c
+++ libft/ft_isupper.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*    ft_isupper.c                                      :+:      :+:    :+:   */
+/*   ft_isupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 11:19:41 by ashu              #+#    #+#             */
-/*   Updated: 2024/08/27 18:38:40 by ashu             ###   ########.fr       */
+/*   Updated: 2024/08/27 19:14:45 by ashu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,4 @@ int ft_isupper(int c)
         return(1);
     }
     return(0);
-}
-
-#include <stdio.h>
-#include <ctype.h>
-int main()
-{
-    printf("%d\n", ft_isupper('A'));
-    printf("%d\n", ft_isupper('a'));
-    printf("%d\n", isupper('A'));
-    printf("%d\n", isupper('a'));
 }
