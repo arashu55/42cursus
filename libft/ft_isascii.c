0ac226cd3745ca79dae41cec83504c16975aa685
Arai Shu <ashu@c3r6s4.42tokyo.jp>
2024-08-29 05:20:37 +0900

    isascii全部作ったお

--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -1,23 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/15 02:51:41 by ashu              #+#    #+#             */
-/*   Updated: 2024/08/29 05:16:36 by ashu             ###   ########.fr       */
+/*   Created: 2024/08/29 04:53:20 by ashu              #+#    #+#             */
+/*   Updated: 2024/08/29 05:06:27 by ashu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#define LIBFT_H
-
-int ft_isupper(int c);
-int ft_islower(int c);
-int	ft_isalpha(int c);
-int ft_isdigit(int c);
-int ft_isalnum(int c);
-int ft_isascii(int c);
-
-#endif
+int ft_isascii(int c)
+{
+    return(c >= 0 && c <= 127);
+}
