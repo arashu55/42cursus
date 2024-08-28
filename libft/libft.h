0a8553db1986c5a2e4c01eee38fa7d07d9846c0b
Arai Shu <ashu@c3r6s4.42tokyo.jp>
2024-08-29 03:52:12 +0900

    ヘッダーファイルにalnum追加

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 02:51:41 by ashu              #+#    #+#             */
-/*   Updated: 2024/08/26 19:24:22 by ashu             ###   ########.fr       */
+/*   Updated: 2024/08/29 03:51:23 by ashu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@ int ft_isupper(int c);
 int ft_islower(int c);
 int	ft_isalpha(int c);
 int ft_isdigit(int c);
+int ft_isalnum(int c);
+
 
 #endif
---
+++ libft/test/test_isalnum.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_isalnum.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/29 03:44:36 by ashu              #+#    #+#             */
+/*   Updated: 2024/08/29 03:46:23 by ashu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <ctype.h>
+
+#define RED "\x1B[31m"
+#define GREEN "\x1B[32m"
+#define RESET "\x1B[0m"
+
+void test_function_name()
+{
+   printf("《ft_isalnum.c》");
+   printf("\n");
+}
+
+void test_isalnum()
+{
+   // int tests[];
+   // int i;
+   // int num;
+   // tests = {-10, -1, 0, ' ', '@', 'A', 'M', 'Z', '[', '`', 'a', 'm', 'z', '{', '}', 127, 128, 150}
+   // i = 0;
+   int tests[] = {
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',  // 全ての数字
+        'A', '-', ' ', '/', ':',                           // 非数字文字と境界
+        '\n', '\t', '\r',                                  // 制御文字
+        '!', '#', '%', '_',                                // よく使う非数字のシンボル
+        -128, -1,                                          // 負の値
+        127, 128, 150, 255                                 // 拡張ASCIIや境界値
+    };
+   int num;
+   int i = 0;
+   num = sizeof(tests) / sizeof(tests[0]);
+
+   while (i != num)
+   {
+      int ft;
+      int honke;  
+      ft = ft_isalnum(tests[i]);
+      honke = isalnum(tests[i]);
+
+      printf("【test%d】", i);
+      if (ft == honke)
+      {
+         printf(GREEN "OK " RESET);
+      }
+      else
+      {
+         printf(RED "FAIELD " RESET);
+      }
+      i++;
+   }
+   printf("\n");
+   printf("\n");
+}
+
+int main() 
+{
+   test_function_name();
+   test_isalnum();
+   return 0;
+}
