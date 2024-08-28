8aa3d79f2e7286107c80e17a4f4ea0522ef473b5
Arai Shu <ashu@c3r6s4.42tokyo.jp>
2024-08-29 03:04:45 +0900

    isdigitのストファイル完成

--- libft/test/test_isdigit.c
+++ libft/test/test_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 19:26:44 by ashu              #+#    #+#             */
-/*   Updated: 2024/08/28 22:50:10 by ashu             ###   ########.fr       */
+/*   Updated: 2024/08/29 03:02:59 by ashu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,60 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int main()
+#define RED "\x1B[31m"
+#define GREEN "\x1B[32m"
+#define RESET "\x1B[0m"
+
+void test_function_name()
 {
-//     int i;
-//     i = 0;
-//     while (i != 127)
-//     {
-//         printf("original %d is %d\n", i, ft_isdigit(i));
-//         printf("my %d is %d\n" , i, isdigit(i));
-//         i++;
-//     }
-//     printf("%d\n", ft_isdigit('0'));
-//     printf("%d\n", ft_isdigit('a'));
-//     printf("%d\n", isdigit('0'));
-//     printf("%d\n", isdigit('a'));
+   printf("《ft_digit.c》");
+   printf("\n");
+}
+
+void test_isdigit()
+{
+   // int tests[];
+   // int i;
+   // int num;
+   // tests = {-10, -1, 0, ' ', '@', 'A', 'M', 'Z', '[', '`', 'a', 'm', 'z', '{', '}', 127, 128, 150}
+   // i = 0;
+   int tests[] = {
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',  // 全ての数字
+        'A', '-', ' ', '/', ':',                           // 非数字文字と境界
+        '\n', '\t', '\r',                                  // 制御文字
+        '!', '#', '%', '_',                                // よく使う非数字のシンボル
+        -128, -1,                                          // 負の値
+        127, 128, 150, 255                                 // 拡張ASCIIや境界値
+    };
+   int num;
+   int i = 0;
+   num = sizeof(tests) / sizeof(tests[0]);
+
+   while (i != num)
+   {
+      int ft;
+      int honke;  
+      ft = ft_isdigit(tests[i]);
+      honke = isdigit(tests[i]);
+
+      printf("【test%d】", i);
+      if (ft == honke)
+      {
+         printf(GREEN "OK " RESET);
+      }
+      else
+      {
+         printf(RED "FAIELD " RESET);
+      }
+      i++;
+   }
+   printf("\n");
+   printf("\n");
+}
+
+int main() 
+{
+   test_function_name();
+   test_isdigit();
+   return 0;
 }
