092222292e9ff0a79f43239c84c6919dc40b1c0a
ashu <ayataka5443@gmail.com>
2024-09-09 23:05:21 +0900

    ft_isprint関連一通り作り終わった

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 02:51:41 by ashu              #+#    #+#             */
-/*   Updated: 2024/08/29 05:16:36 by ashu             ###   ########.fr       */
+/*   Updated: 2024/09/09 23:02:33 by ashu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ int	ft_isalpha(int c);
 int ft_isdigit(int c);
 int ft_isalnum(int c);
 int ft_isascii(int c);
+int ft_isprint(int c);
 
 #endif
