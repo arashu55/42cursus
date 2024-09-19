7f9dc4a1e5c0c5fcba76ff46f566512e8805e1b9
ashu <ayataka5443@gmail.com>
2024-09-19 17:40:16 +0900

    ft_countlenとりあえず作った

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,32 +6,40 @@
 /*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 13:59:20 by ashu              #+#    #+#             */
-/*   Updated: 2024/09/19 14:40:13 by ashu             ###   ########.fr       */
+/*   Updated: 2024/09/19 16:09:49 by ashu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+static int ft_countlen(const char *str, char delim, size_t *length)
+{
+    size_t word_count;
+    size_t i;
+    size_t word_len;
+
+    word_count = 0;
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] == delim)
+        {
+            i++;
+        }
+        else
+        {
+            word_len = 0;
+            while (str[i] != delim)
+            {
+                word_len++;
+                i++;
+            }
+
+            lengths[word_count] = word_len;
+            word_count++;
+        }
+    }
+}
+
 char **split(const char *str, const char *delim)
 {
-    if (str == NULL || delim == NULL)
-    {
-        return NULL
-    }
-
-    int len;
-    char *ptr;
-
-    len = ft_strlen(str);
-
-    ptr = (char *)malloc(len);
-
-    start = 0;
-    i = 0;
-    while(i < str*)
-    {
-        if (ft_strncmp(str[i], delim) == 0)
-        {
-            ft_str
-        }
-
-    }
+    
 }
