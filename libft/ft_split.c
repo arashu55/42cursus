9fd58322dd3839d956a1abf9a5d89fdd50971574
ashu <ayataka5443@gmail.com>
2024-09-19 14:40:35 +0900

    split 断念

---
+++ libft/ft_split.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 13:59:20 by ashu              #+#    #+#             */
+/*   Updated: 2024/09/19 14:40:13 by ashu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char **split(const char *str, const char *delim)
+{
+    if (str == NULL || delim == NULL)
+    {
+        return NULL
+    }
+
+    int len;
+    char *ptr;
+
+    len = ft_strlen(str);
+
+    ptr = (char *)malloc(len);
+
+    start = 0;
+    i = 0;
+    while(i < str*)
+    {
+        if (ft_strncmp(str[i], delim) == 0)
+        {
+            ft_str
+        }
+
+    }
+}
