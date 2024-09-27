698006651addbfa90bd876b4a4db06304b6051c0
ashu <ayataka5443@gmail.com>
2024-09-27 17:01:36 +0900

    mandatory done

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,11 +6,28 @@
 /*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 00:51:06 by ashu              #+#    #+#             */
-/*   Updated: 2024/09/23 01:44:03 by ashu             ###   ########.fr       */
+/*   Updated: 2024/09/25 08:30:53 by ashu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+    size_t i;
+    char *str;
+
+    if (!s || !f)
+        return (NULL);
     
+    str = ft_strdup(s);
+
+    if (!str)
+        return (NULL);
+    
+    i = 0;
+    while (str[i])
+    {
+        str[i] = f(i, str[i]);
+        i++;
+    }
+    return (str);
 }
