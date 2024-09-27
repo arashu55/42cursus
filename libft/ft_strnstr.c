2f0436996a1bd98361bf68bff5c557cd33874237
ashu <ayataka5443@gmail.com>
2024-09-27 17:44:57 +0900

    miss shuusei

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -26,7 +26,7 @@ char *ft_strnstr(const char *haystack, const char *needle, size_t len)
         j = 0;
         while (haystack[i + j] && needle[j] && (i + j) < len && haystack[i + j] == needle[j])
         {
-            j++
+            j++;
         }
         if (needle[j] == '\0')
             return (char *)&haystack[i];
