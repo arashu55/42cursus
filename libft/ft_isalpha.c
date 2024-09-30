3ec002aa4dd73455b3b1759eb33a0b1e69848758
ashu <ayataka5443@gmail.com>
2024-09-30 20:39:28 +0900

    9.30

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -12,6 +12,24 @@
 
 #include "libft.h"
 
+static int ft_isupper(int c)
+{
+    if ('A' <= c && c <= 'Z')
+    {
+        return(1);
+    }
+    return(0);
+}
+
+static int ft_islower(int c)
+{
+    if (c >= 'a' && c <= 'z')
+    {
+        return(1);
+    }
+    return(0);
+}
+
 int	ft_isalpha(int c)
 {
    return (ft_isupper (c)|| ft_islower(c));
