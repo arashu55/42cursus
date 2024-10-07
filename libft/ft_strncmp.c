9f58be21ca1ac2fe492a5d34ca501d96169be0d6
ashu <ayataka5443@gmail.com>
2024-10-07 22:26:32 +0900

    strncmp tester shuusei

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 12:33:26 by ashu              #+#    #+#             */
-/*   Updated: 2024/09/13 07:02:34 by ashu             ###   ########.fr       */
+/*   Updated: 2024/10/07 22:25:31 by ashu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,20 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 
     i = 0;
 
-    while(i < n && s1[i] != '0' && s2[i] != '\0')
+    while (i < n && s1[i] != '\0' && s2[i] != '\0')
     {
-        if (s1[i] - s2[i] < 0)
+        if ((unsigned char)s1[i] != (unsigned char)s2[i])
         {
-            return((unsigned char)s1[i] - (unsigned char)s2[i]);
-        }
-        else if (s1[i] - s2[i] > 0)
-        {
-            return((unsigned char)s1[i] - (unsigned char)s2[i]);
+            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
         }
         i++;
     }
-    return(0);
+
+    // どちらかの文字列が終端に達した場合
+    if (i < n)
+    {
+        return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+    }
+
+    return 0;
 }
