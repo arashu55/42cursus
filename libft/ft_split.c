a8f44e35fe23368885e43abd2d8f9e36411592ef
ashu <ayataka5443@gmail.com>
2024-10-08 19:25:58 +0900

    split tester ok

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 13:59:20 by ashu              #+#    #+#             */
-/*   Updated: 2024/09/19 19:30:34 by ashu             ###   ########.fr       */
+/*   Updated: 2024/10/08 19:25:14 by ashu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,9 @@
 
 static size_t ft_countlen(const char *s, char c, size_t *length)
 {
-    size_t word_count;
-    size_t i;
+    size_t word_count = 0;
+    size_t i = 0;
     size_t word_len;
-
-    word_count = 0;
-    i = 0;
 
     while (s[i])
     {
@@ -35,7 +32,6 @@ static size_t ft_countlen(const char *s, char c, size_t *length)
                 word_len++;
                 i++;
             }
-
             length[word_count] = word_len;
             word_count++;
         }
@@ -43,17 +39,13 @@ static size_t ft_countlen(const char *s, char c, size_t *length)
     return word_count;
 }
 
-static char *ft_wordmemcpy(const char *s, size_t start,size_t len)
+static char *ft_wordmemcpy(const char *s, size_t start, size_t len)
 {
-    char *word;
-
-    word = (char *)malloc(sizeof(char) * (len + 1));
+    char *word = (char *)malloc(sizeof(char) * (len + 1));
     if (!word)
         return NULL;
 
-    size_t i;
-
-    i = 0;
+    size_t i = 0;
     while (i < len)
     {
         word[i] = s[start + i];
@@ -82,23 +74,22 @@ static void ft_memfree(char **result, size_t count)
     free(result);
 }
 
-
 char **ft_split(const char *s, char c)
 {
     size_t word_count;
     char **result;
     size_t i;
-    size_t start;
+    size_t start = 0;
+    size_t s_len = ft_strlen(s);
 
     if (!s)
         return NULL;
 
-    size_t *length = (size_t *)malloc(sizeof(size_t) * (ft_strlen(s) + 1));
+    size_t *length = (size_t *)malloc(sizeof(size_t) * (s_len + 1));
     if (!length)
         return NULL;
 
     word_count = ft_countlen(s, c, length);
-    
     result = ft_wordcountmem(word_count);
 
     if (!result)
@@ -108,19 +99,21 @@ char **ft_split(const char *s, char c)
     }
 
     i = 0;
-    start = 0;
     while (i < word_count)
     {
-        result[i] = ft_wordmemcpy(s, start, length[i]);
+        // 次の単語の開始位置まで区切り文字をスキップする
+        while (s[start] == c && start < s_len)
+            start++;
 
-        if (! result[i])
+        result[i] = ft_wordmemcpy(s, start, length[i]);
+        if (!result[i])
         {
             ft_memfree(result, i);
             free(length);
             return NULL;
         }
 
-        start += length[i];
+        start += length[i];  // 次の単語の開始位置に移動
         i++;
     }
 
@@ -128,3 +121,4 @@ char **ft_split(const char *s, char c)
     free(length);
     return result;
 }
+
