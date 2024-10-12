357a54c84c93ee44c49bcc4a04e0fa570cbbc86e
ashu <ayataka5443@gmail.com>
2024-10-12 15:46:19 +0900

    とりあえずmemmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ashu <ashu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 09:47:02 by ashu              #+#    #+#             */
-/*   Updated: 2024/10/08 21:05:35 by ashu             ###   ########.fr       */
+/*   Updated: 2024/10/12 15:45:35 by ashu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,41 @@ void *ft_memmove(void *dst, const void *src, size_t n)
 {
     unsigned char * a;
     const unsigned char * b;
-    unsigned char *c;
-    size_t i;
 
     if (!dst && !src)
         return NULL;
 
-    c = (unsigned char *)malloc(n);
-    if (c == NULL)
-        return NULL;
-
     a = (unsigned char *)dst;
     b = (const unsigned char *)src;
-    i = 0;
 
-    while (i < n)
+    if (a > b && a < b + n)
     {
-        c[i] = b[i];
-        i++;
+        while (n--)
+            a[n] = b[n];
     }
+    else
+    {
+        size_t i = 0;
+        while (i < n)
+        {
+            a[i] = b[i];
+            i++;
+        }
+    }
+
+
+    // while (i < n)
+    // {
+    //     c[i] = b[i];
+    //     i++;
+    // }
     
-    i = 0;
-    while (i < n)
-    {
-        a[i] = c[i];
-        i++;
-    }
-
-    free(c);
+    // i = 0;
+    // while (i < n)
+    // {
+    //     a[i] = c[i];
+    //     i++;
+    // }
 
     return dst;
 }
